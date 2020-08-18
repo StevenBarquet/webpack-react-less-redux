c9fd4a3e21d1de6ded648147d4e54866afc38359
Steven Barquet <mlmjm123@gmail.com>
2020-08-18 11:26:46 -0500

    error 404 aded

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Development issues: None, all fixed and working for production
 Upgrades: the production server open the browser, fixed some warnings, new script "npm run prod" builds all the dist diles and run de producrion server, eslint adjusted.
 
 Other issues:
--Redux needs a more dynamic structure for combine reducers
+-Redux structure fixed
--- src/javascript/AppContainer.jsx
+++ src/javascript/AppContainer.jsx
@@ -1,15 +1,19 @@
 // ---Dependencys
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // ---Pages
 import HomePage from 'Pages/HomePage';
 import ImageExPage from 'Pages/ImageExPage';
+import Error404Page from 'Pages/Error404Page';
 
 function AppContainer() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/img" component={ImageExPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/img" component={ImageExPage} />
+        <Route exact path="*" component={Error404Page} />
+      </Switch>
     </BrowserRouter>
   );
 }
